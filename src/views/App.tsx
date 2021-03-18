6980eb1b748dafa76f9c89c05aa82ae477c8a8ed
Kuba <>
2021-03-18 17:28:58 +0100

    Initial commit

---
+++ src/views/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  console.log("siema");
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello Frontend App</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
